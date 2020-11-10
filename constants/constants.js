9964c02aaaad2cfcd2342a3c664481562feb3af7
Auliz <joseph.auz77@gmail.com>
2020-11-09 23:06:46 -0600

    fixed

--- constants/constants.js
+++
@@ -1,5 +0,0 @@
-export const BOT_USERNAME = 'SharkBot_OG';
-export const OAUTH_TOKEN = 'oauth:wcvo6aio88x8tqu3qjlfqu7clc9zch';
-export const CHANNEL_NAME = '#WildSideC';
-export const DADJOKE_URL = 'https://icanhazdadjoke.com/'; // this is for the !dadjoke command, comment out if you don't want it.
-export const BLOCKED_WORDS = []; // add your list of blocked words for the bot to auto delete messages with
