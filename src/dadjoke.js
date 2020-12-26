f23b95b568472863e6e5539600f9f5049cee4bf0
Auliz <joseph.auz77@gmail.com>
2020-12-26 13:34:55 -0600

    added new commands, pruning

--- src/dadjoke.js
+++ src/dadjoke.js
@@ -12,14 +12,6 @@ const objReq = {
 };
 const url = 'https://icanhazdadjoke.com/';
 
-// async function dadJoke() {
-//   return fetch('https://icanhazdadjoke.com/', objReq)
-//     .then((resp) => resp.json())
-//     .then((joke) => {
-//       return joke;
-//     });
-// }
-
 let fetchDataFromApi = async () => {
   let response = await fetch(url, objReq);
   let result = await response.json();
@@ -32,17 +24,3 @@ async function caller() {
   console.log(jokes.joke);
 }
 caller();
-
-console.log('====================================');
-console.log(jokes.joke);
-console.log('====================================');
-
-// setTimeout(() => {
-//   caller();
-// }, 1000);
-
-// if (jokes.joke === '') {
-//   console.log('Fetch not fast enough.');
-// } else {
-//   console.log(jokes.joke);
-// }
